904aea8d848b2d916d009da37312edc025d7dbf2
StephanieGdSantos <gomess214@gmail.com>
2025-11-21 22:01:34 -0300

    refactor: exclui métodos que não são utilizados

--- coleta-residuos/Data/Repository/IRepository.cs
+++ coleta-residuos/Data/Repository/IRepository.cs
@@ -4,11 +4,7 @@ namespace coleta_residuos.Data.Repository
 {
     public interface IRepository<TEntity>
     {
-        IEnumerable<TEntity> GetAll();
-
         IEnumerable<TEntity> GetAll(int page, int size);
-
-        IEnumerable<TEntity> GetAllReference(int lastReference, int size);
 
         TEntity GetById(int id);
         void Add(TEntity entity);
